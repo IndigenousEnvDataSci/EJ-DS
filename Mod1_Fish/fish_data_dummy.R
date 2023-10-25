934050ec330aec94f2b11e5b4adc5f7dea049a07
HannahOgrady <ogradyha@gmail.com>
2023-10-25 18:30:05 -0400

    Created fish dummy data
    Created dummy fish mercury data to be used during the creation of the fish module. Note this will be replaced by real data later.

---
+++ Mod1_Fish/fish_data_dummy.R
@@ -0,0 +1,20 @@
+# Currently the FDA says that any fish with >0.46 ug/g mercury is best avoided so I am currently using that boundary as the halmark for a high mercury content. https://www.fda.gov/food/environmental-contaminants-food/technical-information-development-fdaepa-advice-about-eating-fish-those-who-might-become-or-are#:~:text=Any%20fish%20with%20an%20average,week%20%3D%200.23%20%C2%B5g%2Fg. 
+
+#Data related to the relationship between trout length and mercury concentration is based on this article: https://jackson.eeb.utoronto.ca/files/2012/10/Somers-and-Jackson-1993.pdf
+
+# Create a vector of fish lengths
+fish_length <- rnorm(300, mean = 56.5, sd = 11.3)
+  
+# Using an equation to relate length to mercury for the older dataset
+mercury_1998 <- (fish_length * 0.05) + 1.2 + rnorm(100, mean = 0, sd = 0.2)
+
+fish_1998 <- data.frame(length  = fish_length, mercury = mercury_1998)
+# Using an equation to relate length to mercury for the newer data. 
+
+mercury_2023 <- (fish_length * 0.05) + 1.5 + rnorm(100, mean = 0, sd = 0.2)
+
+fish_2023 <- data.frame(length = fish_length, mercury = mercury_2023)
+
+#Saving Data 
+save(fish_1998, file = "Mod1_Fish/fish_1998.RData")
+save(fish_2023, file = "Mod1_Fish/fish_2023.RData")
