9b5545995b23d16f0040a79f008497c9b30704f2
HannahOgrady <ogradyha@gmail.com>
2023-12-06 10:34:35 -0500

    Changed tree-number to tree density and added a bonus section.
    Extra section is just for if students have extra time or want to look back in the future. The tree_density is to clarify what we are describing in the data.

--- Mod4_Forestry/Forestry_data.R
+++ Mod4_Forestry/Forestry_data.R
@@ -10,7 +10,7 @@ set.seed(1)
   ## Inputing data from bulletin
   site_1 <- data.frame(age = seq(20, 160, by = 10), 
     board_feet = c(0, 6200, 17400, 31400, 44500, 56500, 66800, 75700, 83000, 89500, 94700, 99300, 103500, 107800, 110900), 
-    tree_number = c(0, 165, 278, 322, 308, 279, 250, 224, 204, 186, 171, 159, 149, 141, 133))
+    tree_density = c(0, 165, 278, 322, 308, 279, 250, 224, 204, 186, 171, 159, 149, 141, 133))
   ## Jittering data so it isn't a perfect line
   site_1$board_feet <- jitter(site_1$board_feet)
  ## Board feet cannot reasonably be less than 0
@@ -20,7 +20,7 @@ set.seed(1)
   ## Inputing data from bulletin
   site_2 <- data.frame(age = seq(20, 160, by = 10), 
     board_feet = c(4900, 24400, 47000, 70500, 90800, 108500, 124700, 137700, 148900, 157900, 165500, 172000, 178000, 183300, 188100), 
-    tree_number = c(148, 247, 218, 173, 137, 113, 97, 84, 75, 69, 63, 59, 55, 51, 48))
+    tree_density = c(148, 247, 218, 173, 137, 113, 97, 84, 75, 69, 63, 59, 55, 51, 48))
   ## Jittering data so it isn't a perfect line
   site_2$board_feet <- jitter(site_2$board_feet)
   ## Board feet cannot reasonably be less than 0
@@ -30,7 +30,7 @@ set.seed(1)
   ## Inputing data from bulletin
   site_3 <- data.frame(age = seq(20, 160, by = 10), 
     board_feet = c(0, 0, 3800, 9500, 15900, 22100, 27800, 32700, 37200, 41000, 44000, 46900, 49500, 51700, 54000), 
-    tree_number = c(0, 0, 124, 220, 276, 305, 318, 307, 293, 278, 263, 250, 237, 227, 217))
+    tree_density = c(0, 0, 124, 220, 276, 305, 318, 307, 293, 278, 263, 250, 237, 227, 217))
   ## Jittering data so it isn't a perfect line
   site_3$board_feet <- jitter(site_3$board_feet)
   ## Board feet cannot reasonably be less than 0
@@ -40,7 +40,7 @@ set.seed(1)
   ## Inputing data from bulletin
   site_4 <- data.frame(age = seq(20, 160, by = 10), 
     board_feet = c(2800, 18800, 39000, 60000, 79000, 96000, 110900, 123000, 133500, 142000, 149400, 155700, 161300, 166500, 171400), 
-    tree_number = c(119, 254, 269, 231, 190, 158, 136, 118, 106, 95, 87, 80, 75, 71, 67))
+    tree_density = c(119, 254, 269, 231, 190, 158, 136, 118, 106, 95, 87, 80, 75, 71, 67))
   ## Jittering data so it isn't a perfect line
   site_4$board_feet <- jitter(site_4$board_feet)
   ## Board feet cannot reasonably be less than 0
