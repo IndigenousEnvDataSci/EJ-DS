5a125039118d79fed1285fee145c6b6ac9b4f8fd
Rachel Torres <r_torres@ucsb.edu>
2024-05-10 16:16:20 -0700

    made edits to intro about mercury, need to revisit epa threshold

--- Mod1_Fish/fish_data_dummy.R
+++ Mod1_Fish/fish_data_dummy.R
@@ -2,15 +2,16 @@
 
 #Data related to the relationship between trout length and mercury concentration is based on this article: https://jackson.eeb.utoronto.ca/files/2012/10/Somers-and-Jackson-1993.pdf
 
-# Create a vector of fish lengths
-fish_length <- rnorm(300, mean = 56.5, sd = 11.3)
+# Create a vector of fish lengths [cm]
+fish_length <- rnorm(300, mean = 56.5, sd = 11.3) 
   
-# Using an equation to relate length to mercury for the older dataset
+# Using an equation to relate length to mercury for the older dataset 
+# mercury concentration [ug/g]
 mercury_1998 <- (fish_length * 0.05) + 1.2 + rnorm(100, mean = 0, sd = 0.2)
 
 fish_1998 <- data.frame(length  = fish_length, mercury = mercury_1998)
-# Using an equation to relate length to mercury for the newer data. 
 
+# Using an equation to relate length to mercury for the newer data 
 mercury_2023 <- (fish_length * 0.05) + 1.5 + rnorm(100, mean = 0, sd = 0.2)
 
 fish_2023 <- data.frame(length = fish_length, mercury = mercury_2023)
